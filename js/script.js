a4698043eb9c256e5aecb83b56e25c9242a08feb
ghetter <49289242+ghetter@users.noreply.github.com>
2023-09-12 00:36:35 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,34 +1,57 @@
-var flkty = new Flickity( '.main-carousel', {
-    cellAlign: 'left',
-    contain: true,
-    groupCells: 3,
-    freeScroll: true,
-  });
 
-  var flkty = new Flickity( '.fb-carousel', {
-    cellAlign: 'left',
-    contain: true,
-    groupCells: 3,
-    freeScroll: true,
-  });
-
-  var flkty = new Flickity( '.tut-carousel', {
-    cellAlign: 'left',
-    contain: true,
-    groupCells: 3,
-    freeScroll: true,
-  });  
   
+  
+  if (window.matchMedia('(max-width: 1024px)').matches) {
+    var flkty = new Flickity( '.service-carousel', {
+      cellAlign: 'center',
+      contain: true,
+      groupCells: 1,
+    });
+
+    var flkty = new Flickity( '.fb-carousel', {
+      cellAlign: 'center',
+      contain: true,
+      groupCells: 1,
+    }); 
+
+
+    var flkty = new Flickity( '.tut-carousel', {
+      cellAlign: 'center',
+      contain: true,
+      groupCells: 1,
+    }); 
+
+    var flkty = new Flickity( '.main-carousel', {
+      cellAlign: 'center',
+      contain: true,
+      groupCells: 1,
+    });
+
+    
+
+} else {
   var flkty = new Flickity( '.service-carousel', {
     cellAlign: 'left',
     contain: true,
     groupCells: 3,
   });
 
-  if (matchMedia('(max-width: 900px)').matches) {
-    var flkty = new Flickity( '.service-carousel', {
-      cellAlign: 'left',
-      contain: true,
-      groupCells: 1,
-    });
+  var flkty = new Flickity( '.fb-carousel', {
+    cellAlign: 'left',
+    contain: true,
+    groupCells: 3,
+  }); 
+
+  var flkty = new Flickity( '.tut-carousel', {
+    cellAlign: 'left',
+    contain: true,
+    groupCells: 3,
+  }); 
+  
+  var flkty = new Flickity( '.main-carousel', {
+    cellAlign: 'left',
+    contain: true,
+    groupCells: 3,
+  });
+  
 }
