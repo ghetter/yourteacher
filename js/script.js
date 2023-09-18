dd6a4d25bd13bca636481029a6486af90e72101b
ghetter <49289242+ghetter@users.noreply.github.com>
2023-09-18 23:18:03 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
-const body = document.querySelector('body');
+const body = document.querySelector('body'),
 toggle = document.querySelector('.nav__burger'),
-menu = document.querySelector('.nav__list')
+menu = document.querySelector('.nav__list'),
+map_icon = document.querySelector('.page-map__name'),
+map_list = document.querySelector('.page-map__list');
+
+map_icon.addEventListener("click", () => {
+  map_icon.classList.toggle('active');
+  map_list.classList.toggle('active');
+});
 
 toggle.addEventListener("click", () => {
   toggle.classList.toggle("active");
@@ -8,13 +15,32 @@ toggle.addEventListener("click", () => {
   body.classList.toggle('locked');
 });
 
-document.querySelectorAll('.nav__item').forEach(item => {
-  item.addEventListener("click", () => {
-    toggle.classList.toggle("active");
-    menu.classList.toggle('active');
-    body.classList.toggle('locked');
-  });
-})
+document.addEventListener("click", function (event) {
+  const navItems = document.querySelectorAll(".nav__item-has-child");
+  const subnavLists = document.querySelectorAll(".subnav__list");
+
+
+for (let i = 0; i < navItems.length; i++) {
+    if (navItems[i].contains(event.target)) {
+      const subnavList = navItems[i].querySelector(".subnav__list");
+      subnavList.classList.toggle("active");
+    } else {
+      subnavLists[i].classList.remove("active");
+    }
+  }
+});
+
+
+if (window.matchMedia('(max-width: 1279px)').matches) {
+
+  document.querySelectorAll('.subnav__item').forEach(item => {
+    item.addEventListener("click", () => {
+      toggle.classList.toggle("active");
+      menu.classList.toggle('active');
+      body.classList.toggle('locked');
+    });
+  })
+}
   
   if (window.matchMedia('(max-width: 1024px)').matches) {
     var flkty = new Flickity( '.service-carousel', {
