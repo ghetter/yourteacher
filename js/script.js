1b7d5506617dc2362d51d6335515ffc208421eea
ghetter <49289242+ghetter@users.noreply.github.com>
2023-09-21 20:35:25 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ toggle = document.querySelector('.nav__burger'),
 menu = document.querySelector('.nav__list'),
 map_icon = document.querySelector('.page-map__name'),
 map_list = document.querySelector('.page-map__list');
+modal = document.querySelector('.modal');
+modal_close = document.querySelector('.modal__close');
 
 map_icon.addEventListener("click", () => {
   map_icon.classList.toggle('active');
@@ -18,18 +20,27 @@ toggle.addEventListener("click", () => {
 document.addEventListener("click", function (event) {
   const navItems = document.querySelectorAll(".nav__item-has-child");
   const subnavLists = document.querySelectorAll(".subnav__list");
-
-
-for (let i = 0; i < navItems.length; i++) {
-    if (navItems[i].contains(event.target)) {
-      const subnavList = navItems[i].querySelector(".subnav__list");
-      subnavList.classList.toggle("active");
-    } else {
-      subnavLists[i].classList.remove("active");
+  for (let i = 0; i < navItems.length; i++) {
+      if (navItems[i].contains(event.target)) {
+        const subnavList = navItems[i].querySelector(".subnav__list");
+        subnavList.classList.toggle("active");
+      } else {
+        subnavLists[i].classList.remove("active");
+      }
     }
-  }
 });
 
+document.querySelectorAll('.modal-link').forEach(modal_btn => {
+  modal_btn.addEventListener("click", () => {
+    modal.classList.toggle('active');
+    body.classList.toggle('locked');
+  })
+});
+
+modal_close.addEventListener("click", () => {
+  modal.classList.remove('active');
+  body.classList.remove('locked');
+})
 
 if (window.matchMedia('(max-width: 1279px)').matches) {
 
@@ -42,36 +53,7 @@ if (window.matchMedia('(max-width: 1279px)').matches) {
   })
 }
   
-  if (window.matchMedia('(max-width: 1024px)').matches) {
-    var flkty = new Flickity( '.service-carousel', {
-      cellAlign: 'center',
-      contain: true,
-      groupCells: 1,
-    });
-
-    var flkty = new Flickity( '.fb-carousel', {
-      cellAlign: 'center',
-      contain: true,
-      groupCells: 1,
-      adaptiveHeight: true,
-    }); 
-
-
-    var flkty = new Flickity( '.tut-carousel', {
-      cellAlign: 'center',
-      contain: true,
-      groupCells: 1,
-    }); 
-
-    var flkty = new Flickity( '.main-carousel', {
-      cellAlign: 'center',
-      contain: true,
-      groupCells: 1,
-    });
-
-    
-
-} else {
+if (window.matchMedia('(min-width: 1024px)').matches) {
   var flkty = new Flickity( '.service-carousel', {
     cellAlign: 'left',
     contain: true,
@@ -89,6 +71,7 @@ if (window.matchMedia('(max-width: 1279px)').matches) {
     cellAlign: 'left',
     contain: true,
     groupCells: 3,
+    lazyLoad: 1,
   }); 
   
   var flkty = new Flickity( '.main-carousel', {
@@ -96,5 +79,32 @@ if (window.matchMedia('(max-width: 1279px)').matches) {
     contain: true,
     groupCells: 3,
   });
+} else {
+  var flkty = new Flickity( '.service-carousel', {
+    cellAlign: 'center',
+    contain: true,
+    groupCells: 1,
+  });
+
+  var flkty = new Flickity( '.fb-carousel', {
+    cellAlign: 'center',
+    contain: true,
+    groupCells: 1,
+  }); 
+
+
+  var flkty = new Flickity( '.tut-carousel', {
+    cellAlign: 'center',
+    contain: true,
+    groupCells: 1,
+    lazyLoad: 1,
+  }); 
+
+  var flkty = new Flickity( '.main-carousel', {
+    cellAlign: 'center',
+    contain: true,
+    groupCells: 1,
+  });
+
   
 }
